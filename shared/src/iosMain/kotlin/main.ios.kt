b3f9a56a209bff8312b8822f32bb79b7719459e0
Misak <misak@zempro.cz>
2026-04-28 19:18:38 +0200

    Fix iOS keyboard panning issue hiding TopAppBar

--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -1,10 +1,16 @@
 import androidx.compose.ui.window.ComposeUIViewController
+import androidx.compose.ui.uikit.OnFocusBehavior
 import com.zakazky.app.common.models.AppDatabase
 import com.zakazky.app.common.utils.PlatformStorage
 
 actual fun getPlatformName(): String = "iOS"
 
-fun MainViewController() = ComposeUIViewController {
+@OptIn(androidx.compose.ui.ExperimentalComposeUiApi::class)
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        onFocusBehavior = OnFocusBehavior.DoNotPan
+    }
+) {
     AppDatabase.init(PlatformStorage())
     App()
 }
